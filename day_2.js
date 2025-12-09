2b74a5e71d58a1e7b266fc8809c1512644e10861
Ajad Shukla <ajadshukla718@gmail.com>
2025-12-10 03:27:10 +0530

    Add day_2.js  file for practice of variables in js

---
+++ day_2.js
@@ -0,0 +1,20 @@
+let a= "abc";
+let b="";
+let c = null;
+let d;
+
+console.log("value of a = " , a);
+console.log("value of b = " , b);
+console.log("value of c = " , c);
+console.log("value of d = " , d);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+let score  = 33 ;
+console.log(typeof score);
+console.log(typeof ("score"));
+
+score = Number(score);
+console.log(typeof score);
