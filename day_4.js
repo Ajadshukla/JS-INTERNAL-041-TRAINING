4fe5f1872e559db7ce200db683905ee62ddd7381
Ajad Shukla <ajadshukla718@gmail.com>
2025-12-11 21:36:02 +0530

    Add day_4 on my git where i learned date and time through js

---
+++ day_4.js
@@ -0,0 +1,6 @@
+let MyDate = new Date();
+console.log(MyDate.toDateString());
+console.log(MyDate.toTimeString());
+console.log(MyDate.toJSON());
+console.log(MyDate.toUTCString());
+console.log(MyDate.toDateString());
